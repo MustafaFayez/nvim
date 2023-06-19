3658591db6365e06146df285ebb442b7551deb1b
MustafaFayez <mustafa.fayez.ahmed@gmail.com>
2023-06-19 02:22:47 -0700

    lsp python and flash nvim

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -33,9 +33,9 @@ return {
       cmp.setup({
         mapping = {
           ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-f>'] = cmp_action.luasnip_jump_forward(),
           ['<C-b>'] = cmp_action.luasnip_jump_backward(),
@@ -70,10 +70,73 @@ return {
 
       -- (Optional) Configure lua language server for neovim
       require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+      require('lspconfig').pyright.setup({
+        on_attach = lsp.on_attach,
+        -- flags = lsp_flags,
+        settings = {
+          pyright = {
+            disableOrganizeImports = true,
+            disableDiagnostics = false,
+          },
+          python = {
+            analysis = {
+              -- typeCheckingMode = "off",
+              -- diagnosticSeverityOverrides = "none"
+              diagnosticSeverityOverrides = {
+                reportUndefinedVariable = "none",
+                analyzeUnannotatedFunctions = false,
+                strictParameterNoneValue = false,
+                enableTypeIgnoreComments = false,
+                reportMissingModuleSource = "none",
+                reportMissingImports = "none",
+                reportUnusedExpression = "none",
+                reportGeneralTypeIssues = "none"
+              }
+            },
+          },
+        }
+      })
+      -- require('lspconfig').pylint.setup({})
+      require('lspconfig').bashls.setup({})
+      require'lspconfig'.ruff_lsp.setup{
+        init_options = {
+          settings = {
+            -- Any extra CLI arguments for `ruff` go here.
+            args = {},
+          }
+        }
+      }
+      -- require('lspconfig').pylsp.setup {
+      --   filetypes = {'python'},
+      --   settings = {
+      --     configurationSources = {"flake8"},
+      --     formatCommand = {"black"},
+      --     pylsp = {
+      --       plugins = {
+      --         -- jedi_completion = {fuzzy = true},
+      --         -- jedi_completion = {eager=true},
+      --         jedi_completion = {
+      --           include_params = true,
+      --         },
+      --         jedi_signature_help = {enabled = true},
+      --         jedi = {
+      --           extra_paths = {'~/projects/work_odoo/odoo14', '~/projects/work_odoo/odoo14'},
+      --           -- environment = {"odoo"},
+      --         },
+      --         pyflakes={enabled=true},
+      --         -- pylint = {args = {'--ignore=E501,E231', '-'}, enabled=true, debounce=200},
+      --         pylsp_mypy={enabled=false},
+      --         pycodestyle={
+      --           enabled=true,
+      --           ignore={'E501', 'E231', 'E226', 'W291', 'E302'},
+      --           maxLineLength=120},
+      --           yapf={enabled=true}
+      --         }
+      --       }
+      --     }
+      --   }
+        lsp.setup()
+      end
 
-
-      lsp.setup()
-    end
-
+    }
   }
-}
---
+++ lua/plugins/null_ls.lua
@@ -0,0 +1,13 @@
+return {
+--   "jose-elias-alvarez/null-ls.nvim",
+--   event = { "BufReadPre", "BufNewFile" },
+--   config = function ()
+--     local null_ls = require("null-ls")
+--     null_ls.setup({
+--       sources = {
+--         null_ls.builtins.formatting.ruff,
+--         null_ls.builtins.diagnostics.ruff,
+--       }
+--     })
+--   end,
+}
